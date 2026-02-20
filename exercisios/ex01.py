972c0bd9c91064443f773b1f9d5b108bde07d108
kakauzinha-cv <cantelli.kauany@escola.pr.gov.br>
2026-02-20 11:10:26 +0000

    fazendo atv  03

--- exercisios/ex01.py
+++ exercisios/ex01.py
@@ -20,3 +20,19 @@ valores = [200000, 320000, 500000]
 plt.bar(celulares, valores)
 plt.show()
 plt.savefig('./exercisios/ex02.png')
+
+
+
+#3) Identifique na turma qual é o time de cada um e construa um gráfico de barras para mostrar a popularidade cada time.
+#Grêmio: 3; Flamengo: 2; Palmeiras: 1; internacional:2; Vasco: 1
+plt.clf()
+
+times = [' Grẽmio' , 'Flamengo', 'Palmeiras', 'internacional', 'Vasco']
+torcedores = [3, 2, 1, 2, 1]
+cores = ['#FFFFFF', '#FFD700', ' #006437', '#E30613', '#000000']
+
+plt.bar(times, torcedores, color= cores)
+plt.xlabel('Times do campeonato brasieiro')
+plt.ylabel('N° de torcedores')
+plt.show()
+plt.savefig('./exercisios/ex03.png')
